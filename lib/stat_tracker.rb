192c83cdad31bb8ae8c0c25b11203417bfc814dd
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-01-10 14:13:11 -0800

    Fix: update naming

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,16 +1,11 @@
-require 'CSV'
 require_relative './stat_data'
 require_relative './team_stats'
 
 class StatTracker < StatData
-
-  attr_reader :games,
-              :teams, 
-              :game_teams
   
   def initialize(locations)
     super (locations)
-    @team = TeamStats.new(locations)
+    @team_stat = TeamStats.new(locations)
   end
 
   def self.from_csv(locations)
@@ -18,35 +13,34 @@ class StatTracker < StatData
   end
 
   def team_info(team_id)
-    @team.team_info(team_id)
+    @team_stat.team_info(team_id)
   end
 
   def best_season(team_id)
-    @team.best_season(team_id)
+    @team_stat.best_season(team_id)
   end
 
   def worst_season(team_id)
-    @team.worst_season(team_id)
+    @team_stat.worst_season(team_id)
   end
 
   def average_win_percentage(team_id)
-    @team.average_win_percentage(team_id)
+    @team_stat.average_win_percentage(team_id)
   end
 
   def most_goals_scored(team_id)
-    @team.most_goals_scored(team_id)
+    @team_stat.most_goals_scored(team_id)
   end
 
   def fewest_goals_scored(team_id)
-    @team.fewest_goals_scored(team_id)
+    @team_stat.fewest_goals_scored(team_id)
   end
 
   def favorite_opponent(team_id)
-    @team.favorite_opponent(team_id)
+    @team_stat.favorite_opponent(team_id)
   end
 
   def rival(team_id)
-    @team.rival(team_id)
+    @team_stat.rival(team_id)
   end
-   
 end
