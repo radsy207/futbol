2108b4235aed0c8e04c1f1013baf0ca665e57164
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-01-10 12:27:53 -0800

    Fix: update naming

--- lib/team_stats.rb
+++ lib/team_stats.rb
@@ -3,36 +3,32 @@ require_relative './stat_data'
 
 class TeamStats < StatData
 
-    attr_reader :games,
-              :teams, 
-              :game_teams
-
   def initialize(locations)
     super(locations)
   end
 
-  def game_team_info_by_team_id(team_id)
-    team_games = @game_teams.select do |row|
+  def game_teams_data_by_team_id(team_id)
+    game_teams_data_by_team = @game_teams.select do |row|
       row if row[:team_id] == team_id
     end
-    team_games
+    game_teams_data_by_team
   end
 
-  def game_info_by_team_id(team_id)
-    team_game_info = @games.select do |row|
+  def games_data_by_team_id(team_id)
+    games_data_by_team = @games.select do |row|
       row if row[:away_team_id] == team_id || row[:home_team_id] == team_id
     end
-    team_game_info
+    games_data_by_team
   end
 
   def average_win_percentage(team_id)
-    team_games = game_team_info_by_team_id(team_id)
-    team_wins = team_games.count { |row| row[:result] == 'WIN'}
-    (team_wins.to_f/ team_games.count).round(2)
+    game_teams_data_by_team = game_teams_data_by_team_id(team_id)
+    team_wins = game_teams_data_by_team.count { |row| row[:result] == 'WIN'}
+    (team_wins.to_f/ game_teams_data_by_team.count).round(2)
   end
 
   def goals_by_team_id(team_id)
-    goals = game_team_info_by_team_id(team_id).map { |row| row[:goals].to_i}
+    goals = game_teams_data_by_team_id(team_id).map { |row| row[:goals].to_i}
   end
 
   def most_goals_scored(team_id)
@@ -58,12 +54,9 @@ class TeamStats < StatData
   end
 
   def results_by_season(team_id)
-    team_games = game_team_info_by_team_id(team_id)
-    team_game_info = game_info_by_team_id(team_id)
-    
     results_by_season = Hash.new{|k, v| k[v]= []}
-    team_game_info.each do |row1|
-      team_games.each do |row2|
+    games_data_by_team_id(team_id).each do |row1|
+      game_teams_data_by_team_id(team_id).each do |row2|
         results_by_season[row1[:season]] << row2[:result] if row1[:game_id] == row2[:game_id]
       end
     end
@@ -79,17 +72,15 @@ class TeamStats < StatData
   end
 
   def best_season(team_id)
-    season_win_percentage(team_id).max_by{|k, v| v}[0]
+    season_win_percentage(team_id).max_by{|k, v| v}.first
   end
 
   def worst_season(team_id)
-    season_win_percentage(team_id).min_by{|k, v| v}[0]
+    season_win_percentage(team_id).min_by{|k, v| v}.first
   end
 
   def opponent_results(team_id)
-    team_game_ids = game_info_by_team_id(team_id).map do |row|
-      row[:game_id]
-    end
+    team_game_ids = games_data_by_team_id(team_id).map {|row| row[:game_id]}
 
     results_by_team = Hash.new {|hash, key| hash[key] = []}
     @game_teams.each do |row|
@@ -111,7 +102,7 @@ class TeamStats < StatData
 
     favorite_opponent = nil
     @teams.each do |row|
-    favorite_opponent = row[:teamname] if row[:team_id] == favorite_opponent_team.first
+      favorite_opponent = row[:teamname] if row[:team_id] == favorite_opponent_team.first
     end
     favorite_opponent
   end
@@ -121,7 +112,7 @@ class TeamStats < StatData
 
     rival= nil
     @teams.each do |row|
-    rival = row[:teamname] if row[:team_id] == rival_team.first
+      rival = row[:teamname] if row[:team_id] == rival_team.first
     end
     rival
   end
