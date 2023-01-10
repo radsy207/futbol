bbc8042432870d877d3e8360c2c3367971996a64
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-01-10 11:39:07 -0800

    Feat: create #initialize method

---
+++ lib/stat_data.rb
@@ -0,0 +1,11 @@
+require 'CSV'
+
+class StatData
+  attr_reader :games, :teams, :game_teams
+
+  def initialize(locations)
+    @games = CSV.read(locations[:games], headers: true, header_converters: :symbol)
+    @teams = CSV.read(locations[:teams], headers: true, header_converters: :symbol)
+    @game_teams = CSV.read(locations[:game_teams], headers: true, header_converters: :symbol)
+  end
+end
